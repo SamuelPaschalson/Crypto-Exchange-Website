00d9b0f71bda870a39c2035d78a85b0416683454
SamuelPaschalson <samuelpaschalson@gmail.com>
2023-03-23 15:49:48 +0100

    cryptyx

---
+++ README.md
@@ -0,0 +1,42 @@
+<div align="center">
+
+  <h2 align="center">Cryptyx - Cryptocurrency Exchange Website</h2>
+
+  Cryptyx is a fully responsive cryptocurrency exchange website, <br />Responsive for all devices, build using HTML, CSS, and JavaScript.
+
+  <a href="https://cryptyx-exchange.netlify.app/"><strong>➥ Live Demo</strong></a>
+
+</div>
+
+<br />
+
+
+### Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+* [Git](https://git-scm.com/downloads "Download Git") must be installed on your operating system.
+
+### Run Locally
+
+To run **Cryptyx** locally, run this command on your git bash:
+
+Linux and macOS:
+
+```bash
+sudo git clone https://github.com/SamuelPaschalson/Crypto-Exchange-Website.git
+```
+
+Windows:
+
+```bash
+git clone https://github.com/SamuelPaschalson/Crypto-Exchange-Website.git
+```
+
+### Contact
+
+If you want to contact with me you can reach me at [WhatsApp](https://wa.me/2348182540860?text=Hi%20am%20)
+
+### License
+
+This project is **not free to use** and contains license.
---
+++ assets/js/download.js
@@ -0,0 +1,97 @@
+'use strict';
+
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  navToggler.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * redirect download
+ */
+
+function windows8() {
+  window.location.href = "FileZilla_3.63.1_win64_sponsored2-setup.exe";
+}
+
+function macos() {
+  window.location.href = "FileZilla_3.63.1_win64_sponsored2-setup.exe";
+}
+
+
+/**
+ * header active
+ */
+
+const header = document.querySelector("[data-header]");
+
+const activeHeader = function () {
+  if (window.scrollY > 300) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeHeader);
+
+
+
+
+/**
+ * scroll revreal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 1) {
+      sections[i].classList.add("active");
+    } else {
+      sections[i].classList.remove("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
